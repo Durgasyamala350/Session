8b3267d3e55d09c4f0b89ff70e9137d78a766b6f
Durgasyamala350 <Durgasyamala.peruri@prowesssoft.com>
2021-12-07 19:51:11 +0530

    pushing 6-12-21 task

---
+++ Bank.java
@@ -0,0 +1,52 @@
+package Tasks;
+import java.util.*;
+
+public class Bank {
+private	int accountId;
+private	 String accountHoldName;
+private	 String accountType;
+private	 double balance;
+
+	
+	
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+
+	public String getAccountHoldName() {
+		return accountHoldName;
+	}
+
+
+	public void setAccountHoldName(String accountHoldName) {
+		this.accountHoldName = accountHoldName;
+	}
+
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+
+	public double getBalance() {
+		return balance;
+	}
+
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
+	
